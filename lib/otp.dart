54222b7cd8619ee47b210538d675a1447d054de0
Qharny <cannycc1020@gmail.com>
2024-02-25 15:17:33 +0000

    first commit

---
+++ lib/otp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OtpScreen extends StatelessWidget {
+  const OtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
